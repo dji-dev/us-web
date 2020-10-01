3b0e6d59fe9fdfe925d890966064010a3df3e34c
Michael Bullington <michael@bullington.xyz>
2020-10-01 16:56:42 -0400

    add docs to fetch-compose, work on cleanup

--- packages/fetch-compose/lib/index.ts
+++ packages/fetch-compose/lib/index.ts
@@ -1,104 +1,23 @@
 /* eslint-disable no-shadow */
 import fetch from 'cross-fetch'
 import withRetry from '@zeit/fetch-retry'
-import isPlainObject from 'is-plain-object'
 
-type Fetch<T extends RequestInit> = (req: RequestInfo, opts?: Partial<T>) => Promise<Response>
+import withJSON from './withJSON'
+import withThrowNonOk from './withThrowNonOk'
+import withTimeout from './withTimeout'
+import withJWTToken from './withJWTToken'
 
-export interface TimeoutOptions {
-    timeout: number
-}
+import { Fetch } from './types'
 
-const DEFAULT_TIMEOUT = 10000
-
-export class FakeAbortError extends Error {
-    constructor() {
-        super('Request stopped.')
-        this.name = 'FakeAbortError'
-    }
-}
+export { withRetry, withJSON, withThrowNonOk, withTimeout, withJWTToken, Fetch, fetch }
 
 /**
- * Based on this gist:
- * https://gist.github.com/davej/728b20518632d97eef1e5a13bf0d05c7#gistcomment-3232643
+ * Creates an opinionated default for fetch.
+ *
+ * - Automatically transforms a 'body' object into JSON request.
+ * - Throws an error on non-2xx HTTP responses.
+ * - Allows request to specify a timeout, with default of 10s.
  */
-export function withTimeout<T extends RequestInit>(fetch: Fetch<T>): Fetch<T & TimeoutOptions> {
-    return (req, opts = {}) => {
-        const timeoutValue = opts.timeout ?? DEFAULT_TIMEOUT
-
-        const controller = new AbortController()
-        const promise = fetch(req, { ...opts, signal: controller.signal })
-
-        // Make sure to listen to original AbortController if exists.
-        if (opts.signal) {
-            opts.signal.addEventListener('abort', () => controller.abort())
-        }
-
-        // Set a timeout.
-        const timeout = setTimeout(() => controller.abort(), timeoutValue)
-
-        return promise
-            .finally(() => clearTimeout(timeout))
-            .catch((err) => {
-                // fetry-retry treats AbortError a special case,
-                // so if we timed-out, re-throw as a generic error.
-                if (err.toString().includes('AbortError')) {
-                    throw new FakeAbortError()
-                }
-
-                throw err
-            })
-    }
-}
-
-export interface JSONOptions {
-    body: any
-}
-
-/**
- * Automatically transforms a plain object in the body to JSON.
- */
-export function withJSON<T extends RequestInit>(fetch: Fetch<T>): Fetch<T & JSONOptions> {
-    return (req, opts) => {
-        if (!opts) {
-            return fetch(req, opts)
-        }
-
-        let { body, headers = {}, ...rest } = opts
-
-        // Check if we're dealing with a JSON object.
-        if (isPlainObject(body)) {
-            body = JSON.stringify(body)
-
-            // Normalize headers.
-            headers = new Headers(headers)
-            headers.set('Content-Type', headers.get('Content-Type') ?? 'application/json')
-        }
-
-        // @ts-ignore
-        return fetch(req, {
-            body,
-            headers,
-            ...rest,
-        })
-    }
-}
-
-export function withThrowNonOk<T extends RequestInit>(fetch: Fetch<T>): Fetch<T> {
-    return async (req, opts) => {
-        const res = await fetch(req, opts)
-
-        // Make sure any non-200 responses are thrown.
-        if (!res.ok) {
-            throw new Error(`${res.status} : ${res.statusText}`)
-        }
-
-        return res
-    }
-}
-
-export { withRetry }
-
 export default function createFetch(fetchVal?: Fetch<RequestInit>) {
     return withJSON(withThrowNonOk(withTimeout(fetchVal ?? fetch)))
 }
---
+++ packages/fetch-compose/lib/withJWTToken.ts
@@ -0,0 +1,82 @@
+import { Fetch } from './types'
+
+const UNAUTHORIZED = 401
+
+interface CredentialOptions {
+    skipRefresh?: boolean
+}
+
+type CredentialProvider = {
+    baseHostname: string
+    /**
+     * Gets the current JWT token.
+     */
+    getToken: () => Promise<string>
+    /**
+     * Refresh the JWT token using the refresh token.
+     *
+     * **MUST** throw an error on unsuccessful refresh for the higher-order function
+     * to work properly.
+     */
+    refreshToken: () => Promise<void>
+}
+
+/**
+ * Handles adding the bearer token for authorized routes, otherwise
+ * attempting to refresh the JWT token from backend.
+ *
+ * Requires an implementation of {@type CredentialProvider}
+ *
+ * Allows an optional field named 'skipRefresh'.
+ */
+export default function withJWTToken<T extends RequestInit>(
+    fetch: Fetch<T>,
+    credentialProvider: CredentialProvider
+): Fetch<T & CredentialOptions> {
+    const func: Fetch<T & CredentialOptions> = async (req, opts = {}) => {
+        const baseHostname = new URL(credentialProvider.baseHostname).hostname
+
+        const url = typeof req === 'string' ? req : req.url
+        const hostname = new URL(url).hostname
+
+        // If we're not at the same hostname as the backend, then use normal fetch.
+        if (hostname !== baseHostname) {
+            return await fetch(req, opts)
+        }
+
+        const accessToken = await credentialProvider.getToken()
+        // If we don't have any credentials (or account is guest), then just use normal fetch.
+        if (!accessToken) {
+            return await fetch(req, opts)
+        }
+
+        let { headers = {}, ...rest } = opts
+        // Normalize headers.
+        headers = new Headers(headers)
+        headers.set('Authorization', headers.get('Authorization') ?? `Bearer ${accessToken}`)
+
+        // @ts-ignore
+        const res = await fetch(req, {
+            headers,
+            ...rest,
+        })
+
+        // Try to refresh token.
+        if (!res.ok && !opts.skipRefresh && res.status === UNAUTHORIZED) {
+            await credentialProvider.refreshToken()
+
+            // Retry the request again with new token.
+            //
+            // Usually this could cause infinite recursion, however refreshToken
+            // will throw on any bad response.
+            return await func(req, {
+                ...opts,
+                skipRefresh: true,
+            })
+        }
+
+        return res
+    }
+
+    return func
+}
